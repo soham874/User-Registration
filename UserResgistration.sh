07b37da888e048acd3da9757441515e2bcbd46f2
soham874 <soham874@gmail.com>
2020-11-25 23:25:23 +0530

    Refactored : Added email verification and modified code

--- UserResgistration.sh
+++ UserResgistration.sh
@@ -3,19 +3,28 @@
 shopt -s extglob
 read -p "Enter user 1st name : " first
 read -p "Enter user last name : " last
+read -p "Enter user email ID : " email
+
+function check(){
+	if [[ $1 =~ $2 ]]
+	then
+   	echo "Entry is valid."
+	else
+   	echo "Entry is invalid."
+	fi
+
+}
 
 patfn='^[[:upper:]]{1}[[:lower:]]{2,}$'
+patemail='^[a-z0-9]+([._+-][a-z0-9]+)*(@)[a-zA-Z]+[.]{1}[a-z]{2,3}([.][a-z]{2})*$'
 
-if [[ $first =~ $patfn ]]
-then
-	echo "Valid user 1st name input."
-else
-	echo "Invalid user 1st name. Please retry."
-fi
+echo "First name check:"
+check $first $patfn
+echo "Last name check:"
+check $last $patfn
+echo "Email ID check:"
+check $email $patfn
 
-if [[ $last =~ $patfn ]]
-then
-	echo "Valid user last name input."
-else
-	echo "Invalid user last name. Please retry."
-fi
+
+
+
