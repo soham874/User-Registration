0c3e40129d8aec188261ef3f854dc6c61c673cf7
soham874 <soham874@gmail.com>
2020-11-25 23:38:02 +0530

    Added : Password verification > minimum 8 charecters length

--- UserResgistration.sh
+++ UserResgistration.sh
@@ -15,10 +15,13 @@ read -p "Enter user 1st name : " first
 read -p "Enter user last name : " last
 read -p "Enter user email ID : " email
 read -p "Enter phone number : " phone
+read -p "Enter user password : " password
 
 patfn='^[[:upper:]]{1}[[:lower:]]{2,}$'
 patemail='^[a-z0-9]+([._+-][a-z0-9]+)*(@)[a-zA-Z]+[.]{1}[a-z]{2,3}([.][a-z]{2})*$'
 patphone='^[0-9]{2} [0-9]{10}$'
+
+patpassword='^.{8,}$'
 
 echo "First name check:"
 check $first $patfn
@@ -28,3 +31,5 @@ echo "Email ID check:"
 check $email $patfn
 echo "Phone number check:"
 check $phone $patphone
+echo "Password check for minimum 8 charecters length:"
+check $password $patpassword
