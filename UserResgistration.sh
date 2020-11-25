a96e405b108eb4b013ecde4858688d57b0c24c0f
soham874 <soham874@gmail.com>
2020-11-25 23:15:15 +0530

    Added : Last name validity check

--- UserResgistration.sh
+++ UserResgistration.sh
@@ -2,6 +2,7 @@
 
 shopt -s extglob
 read -p "Enter user 1st name : " first
+read -p "Enter user last name : " last
 
 patfn='^[[:upper:]]{1}[[:lower:]]{2,}$'
 
@@ -12,4 +13,9 @@ else
 	echo "Invalid user 1st name. Please retry."
 fi
 
-
+if [[ $last =~ $patfn ]]
+then
+	echo "Valid user last name input."
+else
+	echo "Invalid user last name. Please retry."
+fi
