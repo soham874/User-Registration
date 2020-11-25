00cec6daf104d7accff042e0629639c13ac02a16
soham874 <soham874@gmail.com>
2020-11-25 23:06:10 +0530

    Refactored : Removed welcome message and added 1st user name check

--- UserResgistration.sh
+++ UserResgistration.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-echo "Hello!!"
+shopt -s extglob
+read -p "Enter user 1st name : " first
+
+patfn='^[[:upper:]]{1}[[:lower:]]{2,}$'
+
+if [[ $first =~ $patfn ]]
+then
+	echo "Valid user 1st name input."
+else
+	echo "Invalid user 1st name. Please retry."
+fi
+
+
