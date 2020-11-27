60f902d2b7486df829303c66d82e4503126b5552
soham874 <soham874@gmail.com>
2020-11-27 15:59:59 +0530

    Refactored : modified code to work with bash

--- UserResgistration.sh
+++ UserResgistration.sh
@@ -11,41 +11,54 @@ function check(){
 	fi
 }
 
+#~~~~~~~~~~~~~first name check~~~~~~~~~~~~
+
 read -p "Enter user 1st name : " first
+
+patname='^[[:upper:]]{1}[[:lower:]]{2,}$'
+echo "##First name check:##"
+check $first $patname
+
+#~~~~~~~~~~~~~last name check~~~~~~~~~~~~
+
 read -p "Enter user last name : " last
+
+echo "##Last name check:##"
+check $last $patname
+
+#~~~~~~~~~~~~~Email ID check~~~~~~~~~~~~~
+
 read -p "Enter user email ID : " email
-read -p "Enter phone number : " phone
-read -p "Enter user password : " password
 
-patfn='^[[:upper:]]{1}[[:lower:]]{2,}$'
-patemail='^[a-z0-9]+([._+-][a-z0-9]+)*(@)[0-9a-zA-Z]+[.]{1}[a-z]{2,3}([.][a-z]{2})?$'
-patphone='^[0-9]{2} [0-9]{10}$'
+patemail='^[a-z0-9]+([._+-][a-z0-9]+)*(@)[0-9a-zA-Z]+[.]{1}[a-z]{2,3}'
+patemailf1='([.][a-z]{2})'
+patemailf2='$'
 
-patpassword='^(?=.*[A-Z]{1,})(?=.*[0-9]{1}).{8,}$'	#will check for minimum 1 Uppercase, minimum 1 digit and minimum length 8
-patspeccheck1='^.*[\W].*$'				#will return true if at least 1 special charecter exists
-patspeccheck2='^(.*?[\W].*?){2,}$'			#will return true if at least 2 special charecters exist
-
-echo "First name check:"
-check $first $patfn
-echo "Last name check:"
-check $last $patfn
-echo "Email ID check:"
-check $email $patemail
-echo "Phone number check:"
-check $phone $patphone
-echo "Password check for minimum 8 charecters length, at least 1 UpperCase charecter, at least 1 numeric charecter, and exactly 1 special charecter :"
-
-if [[ $password =~ $patpassword ]]	#verifying first that length, uppercase and numerical conditions are satisfied
+echo "##Email ID check:##"
+if [[ $email =~ $patemail$patemailf1$patemailf2 || $email =~ $patemail$patemailf2 ]]
 then
-	if [[ $password =~ $patspeccheck2 ]]				#if this case is successful it means there are 2 or more special charecters
-	then
-		echo "Password is invalid. Please try again"
-	elif [[ $password =~ $patspeccheck1 ]]			#if case 1 is false and this is true, it means there is exactly 1 special char.
-	then
-		echo "Password is valid."
-	else						#if both cases fail it means that there are no special charecter
-    		echo "Password is invalid. Please try again"
-	fi
+    echo "Entry is valid."
 else
-	echo "Password is invalid. Please try again"
+    echo "Entry is invalid."
+fi
+
+#~~~~~~~~~~~~~Phone number check~~~~~~~~~~~~~
+
+read -p "Enter phone number : " phone
+patphone='^[[:digit:]]{2}([ ])[[:digit:]]{10}$'
+
+echo "##Phone number check:##"
+check $phone $patphone
+
+#~~~~~~~~~~~~~Password check~~~~~~~~~~~~~~~~~
+
+read -p "Enter user password : " password
+pattern='^([^.,:;!@#$%^&*_+=]*)[.,:;!@#$%^&*_+=]([^.,:;!@#$%^&*_+=]*)$'
+
+echo "##Password check for minimum 8 charecters length, at least 1 UpperCase charecter, at least 1 numeric charecter, and exactly 1 special charecter :##"
+if [[ ${#password} -ge 8 && "$password" == *[[:upper:]]* && "$password" == *[[:lower:]]* && "$password" == *[[:digit:]]* && "$password" =~ $pattern ]]
+then
+	echo "Entry is valid."
+else
+	echo "Entry is invalid."
 fi
